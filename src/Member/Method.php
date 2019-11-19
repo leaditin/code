d18180e0042c775d03d555e45960130714784d99
Igor Vuckovic <igor.vuckovic@arbor-education.com>
2019-11-19 12:16:40 +0100

    Added Method API

---
+++ src/Member/Method.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace Leaditin\Code\Member;
+
+use InvalidArgumentException;
+use Leaditin\Code\DocBlock;
+use Leaditin\Code\Flag;
+use Leaditin\Code\Type;
+use Leaditin\Code\Visibility;
+
+/**
+ * @package Leaditin\Code
+ * @author Igor Vuckovic <igor@vuckovic.biz>
+ * @license MIT
+ */
+class Method
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var Flag
+     */
+    protected $flag;
+
+    /**
+     * @var Argument[]
+     */
+    protected $arguments;
+
+    /**
+     * @var null|string
+     */
+    protected $body;
+
+    /**
+     * @var null|DocBlock
+     */
+    protected $docBlock;
+
+    /**
+     * @var null|Type
+     */
+    protected $returnType;
+
+    /**
+     *
+     * @param string $name
+     * @param null|Flag $flag
+     * @param null|Argument[] $arguments
+     * @param null|string $body
+     * @param null|DocBlock $docBlock
+     * @param null|Type $returnType
+     */
+    public function __construct(
+        string $name,
+        Flag $flag = null,
+        array $arguments = null,
+        string $body = null,
+        DocBlock $docBlock = null,
+        Type $returnType = null
+    ) {
+        $this->name = $name;
+        $this->setFlag($flag);
+        $this->setArguments($arguments ?? []);
+        $this->body = $body;
+        $this->docBlock = $docBlock;
+        $this->returnType = $returnType;
+    }
+
+    /**
+     * @return string
+     */
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return Argument[]
+     */
+    public function arguments(): array
+    {
+        return $this->arguments;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function body(): ?string
+    {
+        return $this->body;
+    }
+
+    /**
+     * @return null|DocBlock
+     */
+    public function docBlock(): ?DocBlock
+    {
+        return $this->docBlock;
+    }
+
+    /**
+     * @return null|Type
+     */
+    public function returnType(): ?Type
+    {
+        return $this->returnType;
+    }
+
+    /**
+     * @param bool $isAbstract
+     */
+    public function setAbstract(bool $isAbstract): void
+    {
+        $isAbstract ? $this->flag->addFlag(Flag::FLAG_ABSTRACT) : $this->flag->removeFlag(Flag::FLAG_ABSTRACT);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAbstract(): bool
+    {
+        return $this->flag->hasFlag(Flag::FLAG_ABSTRACT);
+    }
+
+    /**
+     * @param bool $isFinal
+     */
+    public function setFinal(bool $isFinal): void
+    {
+        $isFinal ? $this->flag->addFlag(Flag::FLAG_FINAL) : $this->flag->removeFlag(Flag::FLAG_FINAL);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isFinal(): bool
+    {
+        return $this->flag->hasFlag(Flag::FLAG_FINAL);
+    }
+
+    /**
+     * @param bool $isStatic
+     */
+    public function setStatic(bool $isStatic): void
+    {
+        $isStatic ? $this->flag->addFlag(Flag::FLAG_STATIC) : $this->flag->removeFlag(Flag::FLAG_STATIC);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isStatic(): bool
+    {
+        return $this->flag->hasFlag(Flag::FLAG_STATIC);
+    }
+
+    /**
+     * @return Visibility
+     */
+    public function visibility(): Visibility
+    {
+        return Visibility::fromFlag($this->flag);
+    }
+
+    /**
+     * @param null|Flag $flag
+     */
+    protected function setFlag(?Flag $flag): void
+    {
+        if ($flag === null) {
+            $flag = new Flag(Flag::FLAG_PUBLIC);
+        }
+
+        $this->flag = $flag;
+    }
+
+    /**
+     * @param Argument[] $arguments
+     */
+    protected function setArguments(array $arguments): void
+    {
+        $this->arguments = [];
+
+        foreach ($arguments as $argument) {
+            if (!$argument instanceof Argument) {
+                throw new InvalidArgumentException(sprintf('Argument must be instance of %s', Argument::class));
+            }
+
+            $this->arguments[] = $argument;
+        }
+    }
+}
---
+++ tests/Member/MethodTest.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Test\Leaditin\Code\Member;
+
+use InvalidArgumentException;
+use Leaditin\Code\DocBlock;
+use Leaditin\Code\Flag;
+use Leaditin\Code\Member\Argument;
+use Leaditin\Code\Member\Method;
+use Leaditin\Code\Type;
+use Leaditin\Code\Visibility;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @package Test\Leaditin\Code
+ * @author Igor Vuckovic <igor@vuckovic.biz>
+ *
+ * @covers \Leaditin\Code\Member\Method
+ */
+final class MethodTest extends TestCase
+{
+    public function constructArgumentsDataProvider(): array
+    {
+        return [
+            [
+                'name' => $this->anything()->toString(),
+                'flag' => new Flag(Flag::FLAG_PUBLIC),
+                'arguments' => null,
+                'body' => $this->anything()->toString(),
+                'docBlock' => null,
+                'returnType' => null,
+            ],
+            [
+                'name' => $this->anything()->toString(),
+                'flag' => null,
+                'arguments' => [
+                    $this->createMock(Argument::class),
+                    $this->createMock(Argument::class),
+                ],
+                'body' => $this->anything()->toString(),
+                'docBlock' => $this->createMock(DocBlock::class),
+                'returnType' => $this->createMock(Type::class),
+            ],
+
+        ];
+    }
+
+    /**
+     * @dataProvider constructArgumentsDataProvider
+     *
+     * @param string $name
+     * @param null|Flag $flag
+     * @param null|array $arguments
+     * @param string $body
+     * @param null|DocBlock $docBlock
+     * @param null|Type $returnType
+     */
+    public function testGetters(string $name, ?Flag $flag, ?array $arguments, string $body, ?DocBlock $docBlock, ?Type $returnType): void
+    {
+        $method = new Method($name, $flag, $arguments, $body, $docBlock, $returnType);
+
+        $this->assertSame($name, $method->name());
+        $this->assertSame((array)$arguments, $method->arguments());
+        $this->assertSame($body, $method->body());
+        $this->assertSame($docBlock, $method->docBlock());
+        $this->assertSame($returnType, $method->returnType());
+    }
+
+    public function testFinalMethod(): void
+    {
+        $method = new Method($this->anything()->toString());
+        $method->setFinal(true);
+
+        $this->assertTrue($method->isFinal());
+
+        $method = new Method($this->anything()->toString(), new Flag(Flag::FLAG_FINAL));
+        $this->assertTrue($method->isFinal());
+
+        $method->setFinal(false);
+        $this->assertFalse($method->isFinal());
+    }
+
+    public function testAbstractMethod(): void
+    {
+        $method = new Method($this->anything()->toString());
+        $method->setAbstract(true);
+
+        $this->assertTrue($method->isAbstract());
+
+        $method = new Method($this->anything()->toString(), new Flag(Flag::FLAG_ABSTRACT));
+        $this->assertTrue($method->isAbstract());
+
+        $method->setAbstract(false);
+        $this->assertFalse($method->isAbstract());
+    }
+
+    public function testStaticMethod(): void
+    {
+        $method = new Method($this->anything()->toString());
+        $method->setStatic(true);
+
+        $this->assertTrue($method->isStatic());
+
+        $method = new Method($this->anything()->toString(), new Flag(Flag::FLAG_STATIC));
+        $this->assertTrue($method->isStatic());
+
+        $method->setStatic(false);
+        $this->assertFalse($method->isStatic());
+    }
+
+    public function visibilityDataProvider(): array
+    {
+        return [
+            [
+                'flag' => null,
+                'visibility' => Visibility::VISIBILITY_PUBLIC,
+            ], [
+                'flag' => new Flag(Flag::FLAG_PUBLIC),
+                'visibility' => Visibility::VISIBILITY_PUBLIC,
+            ],
+            [
+                'flag' => new Flag(Flag::FLAG_PROTECTED),
+                'visibility' => Visibility::VISIBILITY_PROTECTED,
+            ],
+            [
+                'flag' => new Flag(Flag::FLAG_PRIVATE),
+                'visibility' => Visibility::VISIBILITY_PRIVATE,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider visibilityDataProvider
+     *
+     * @param null|Flag $flag
+     * @param string $visibility
+     */
+    public function testVisibility(?Flag $flag, string $visibility): void
+    {
+        $method = new Method($this->anything()->toString(), $flag);
+
+        $this->assertSame($visibility, $method->visibility()->visibility());
+    }
+
+    public function testInvalidArgumentThrowsException(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage(sprintf('Argument must be instance of %s', Argument::class));
+
+        new Method($this->anything()->toString(), null, [$this->anything()->toString()]);
+    }
+}
