59b801bd21fe2e2f338cbc58bb8a185d05509bb1
Igor Vuckovic <igor.vuckovic@arbor-education.com>
2019-11-19 15:03:57 +0100

    Added Type Generator

---
+++ src/Generator/TypeGenerator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Leaditin\Code\Generator;
+
+use Leaditin\Code\Type;
+
+/**
+ * @package Leaditin\Code
+ * @author Igor Vuckovic <igor@vuckovic.biz>
+ * @license MIT
+ */
+class TypeGenerator extends Generator
+{
+    /**
+     * @param Type $type
+     *
+     * @return string
+     */
+    public function generate(Type $type): string
+    {
+        if ($type->type() === Type::TYPE_MIXED) {
+            return '';
+        }
+
+        if ($type->type() === Type::TYPE_VOID) {
+            return 'void';
+        }
+
+        $nullable = $type->isNullable() ? '?' : '';
+
+        if ($type->type() === Type::TYPE_CALLABLE || $type->isScalar()) {
+            return $nullable . $type->type();
+        }
+
+        return $nullable . '\\' . ltrim($type->type(), '\\');
+    }
+}
---
+++ tests/Generator/TypeGeneratorTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Test\Leaditin\Code\Generator;
+
+use Leaditin\Code\Generator\Factory;
+use Leaditin\Code\Generator\TypeGenerator;
+use Leaditin\Code\Type;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @package Test\Leaditin\Code
+ * @author Igor Vuckovic <igor@vuckovic.biz>
+ *
+ * @covers \Leaditin\Code\Generator\TypeGenerator
+ */
+final class TypeGeneratorTest extends TestCase
+{
+    /**
+     * @var TypeGenerator
+     */
+    private $generator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->generator = (new Factory())->typeGenerator();
+    }
+
+    public function typeDataProvider(): array
+    {
+        return [
+            [
+                'type' => new Type(Type::TYPE_BOOL, false),
+                'expected' => 'bool'
+            ],
+            [
+                'type' => new Type(Type::TYPE_BOOL, true),
+                'expected' => '?bool'
+            ],
+            [
+                'type' => new Type(Type::TYPE_INT, false),
+                'expected' => 'int'
+            ],
+            [
+                'type' => new Type(Type::TYPE_INT, true),
+                'expected' => '?int'
+            ],
+            [
+                'type' => new Type(Type::TYPE_FLOAT, false),
+                'expected' => 'float'
+            ],
+            [
+                'type' => new Type(Type::TYPE_FLOAT, true),
+                'expected' => '?float'
+            ],
+            [
+                'type' => new Type(Type::TYPE_STRING, false),
+                'expected' => 'string'
+            ],
+            [
+                'type' => new Type(Type::TYPE_STRING, true),
+                'expected' => '?string'
+            ],
+            [
+                'type' => new Type(Type::TYPE_ARRAY, false),
+                'expected' => 'array'
+            ],
+            [
+                'type' => new Type(Type::TYPE_ARRAY, true),
+                'expected' => '?array'
+            ],
+            [
+                'type' => new Type(Type::TYPE_CALLABLE, false),
+                'expected' => 'callable'
+            ],
+            [
+                'type' => new Type(Type::TYPE_CALLABLE, true),
+                'expected' => '?callable'
+            ],
+            [
+                'type' => new Type(Type::TYPE_MIXED, false),
+                'expected' => ''
+            ],
+            [
+                'type' => new Type(Type::TYPE_MIXED, true),
+                'expected' => ''
+            ],
+            [
+                'type' => new Type(Type::TYPE_VOID, false),
+                'expected' => 'void'
+            ],
+            [
+                'type' => new Type(Type::TYPE_VOID, true),
+                'expected' => 'void'
+            ],
+            [
+                'type' => new Type(self::class, false),
+                'expected' => '\\' . self::class
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider typeDataProvider
+     *
+     * @param Type $type
+     * @param string $expected
+     */
+    public function testGenerate(Type $type, string $expected): void
+    {
+        $this->assertSame($expected, $this->generator->generate($type));
+    }
+}
