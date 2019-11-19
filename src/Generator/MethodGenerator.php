a3f9f847aec8a3d9ec5be40ae6f29cc324b09d0a
Igor Vuckovic <igor.vuckovic@arbor-education.com>
2019-11-19 15:07:18 +0100

    Added Method Generator

---
+++ src/Generator/MethodGenerator.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Leaditin\Code\Generator;
+
+use Leaditin\Code\DocBlock;
+use Leaditin\Code\Member\Method;
+use Leaditin\Code\Tag;
+
+/**
+ * @package Leaditin\Code
+ * @author Igor Vuckovic <igor@vuckovic.biz>
+ * @license MIT
+ */
+class MethodGenerator extends Generator
+{
+    /**
+     * @var ArgumentGenerator
+     */
+    protected $argumentGenerator;
+
+    /**
+     * @var DocBlockGenerator
+     */
+    protected $docBlockGenerator;
+
+    /**
+     * @var TypeGenerator
+     */
+    protected $typeGenerator;
+
+    /**
+     * @var VisibilityGenerator
+     */
+    protected $visibilityGenerator;
+
+    /**
+     * @param ArgumentGenerator $argumentGenerator
+     * @param DocBlockGenerator $docBlockGenerator
+     * @param TypeGenerator $typeGenerator
+     * @param VisibilityGenerator $visibilityGenerator
+     */
+    public function __construct(
+        ArgumentGenerator $argumentGenerator,
+        DocBlockGenerator $docBlockGenerator,
+        TypeGenerator $typeGenerator,
+        VisibilityGenerator $visibilityGenerator
+    ) {
+        $this->argumentGenerator = $argumentGenerator;
+        $this->docBlockGenerator = $docBlockGenerator;
+        $this->typeGenerator = $typeGenerator;
+        $this->visibilityGenerator = $visibilityGenerator;
+
+        $this->setDepth(1);
+        $this->docBlockGenerator->setDepth(1);
+    }
+
+    /**
+     * @param Method $method
+     *
+     * @return string
+     */
+    public function generate(Method $method): string
+    {
+        $output = $this->generateLine($this->docBlockGenerator->generate($this->generateDocBlock($method)), 0);
+
+        $line = '';
+        if ($method->isAbstract()) {
+            $line .= 'abstract ';
+        } elseif ($method->isFinal()) {
+            $line .= 'final ';
+        }
+
+        $line .= $this->visibilityGenerator->generate($method->visibility());
+
+        if ($method->isStatic()) {
+            $line .= ' static';
+        }
+
+        $line .= ' function ' . $method->name() . '(';
+
+        $arguments = [];
+        foreach ($method->arguments() as $argument) {
+            $arguments[] = $this->argumentGenerator->generate($argument);
+        }
+        $line .= implode(', ', $arguments);
+
+        $line .= ')';
+
+        if ($method->returnType() !== null) {
+            $line .= ': ' . $this->typeGenerator->generate($method->returnType());
+        }
+
+        if ($method->isAbstract()) {
+            $output .= $this->generateLine($line . ';');
+
+            return rtrim($output, $this->endOfLine);
+        }
+
+        $output .= $this->generateLine($line);
+        $output .= $this->generateLine('{');
+
+        if ($method->body() !== null) {
+            $output .= $this->generateLine(
+                preg_replace(
+                    '#^((?![a-zA-Z0-9_-]+;).+?)$#m',
+                    '$1',
+                    trim($method->body())
+                ),
+                2
+            );
+        }
+
+        $output .= $this->generateLine('}');
+
+        return rtrim($output, $this->endOfLine);
+    }
+
+    /**
+     * @param Method $method
+     *
+     * @return DocBlock
+     */
+    protected function generateDocBlock(Method $method): DocBlock
+    {
+        $docblock = '';
+        $shortDescription = null;
+        $longDescription = null;
+        $tags = [];
+
+        if ($method->docBlock() !== null) {
+            $docblock = $this->docBlockGenerator->generate($method->docBlock());
+            $shortDescription = $method->docBlock()->shortDescription();
+            $longDescription = $method->docBlock()->longDescription();
+            $tags = $method->docBlock()->tags();
+        }
+
+        foreach ($method->arguments() as $argument) {
+            $comment = '@property ' . $this->argumentGenerator->generate($argument);
+
+            if (strpos($docblock, $comment) === false) {
+                $tags[] = new Tag('property', $this->argumentGenerator->generate($argument));
+            }
+        }
+
+        if ($method->returnType() !== null && strpos($docblock, '@return') === false) {
+            $tags[] = new Tag('return', ($method->returnType()->isNullable() ? 'null|' : '') . $method->returnType()->type());
+        }
+
+        return new DocBlock($shortDescription, $longDescription, $tags);
+    }
+}
---
+++ tests/Generator/MethodGeneratorTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Test\Leaditin\Code\Generator;
+
+use InvalidArgumentException;
+use Leaditin\Code\DocBlock;
+use Leaditin\Code\Flag;
+use Leaditin\Code\Generator\Factory;
+use Leaditin\Code\Generator\MethodGenerator;
+use Leaditin\Code\Member\Argument;
+use Leaditin\Code\Member\Method;
+use Leaditin\Code\Tag;
+use Leaditin\Code\Type;
+use Leaditin\Code\Value;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @package Test\Leaditin\Code
+ * @author Igor Vuckovic <igor@vuckovic.biz>
+ *
+ * @covers \Leaditin\Code\Generator\MethodGenerator
+ * @covers \Leaditin\Code\Generator\MethodGenerator
+ */
+final class MethodGeneratorTest extends TestCase
+{
+    /**
+     * @var MethodGenerator
+     */
+    private $generator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->generator = (new Factory())->methodGenerator();
+    }
+
+    public function testMethodWithArguments(): void
+    {
+        $method = new Method(
+            'doSomething',
+            new Flag(Flag::FLAG_FINAL),
+            [
+                new Argument('name', new Type(Type::TYPE_STRING), new Value()),
+                new Argument('email', new Type(Type::TYPE_STRING), new Value()),
+            ],
+            'return "@author $name <@email>";',
+            null,
+            new Type(Type::TYPE_STRING)
+        );
+
+        $expected = <<<EOL
+    /**
+     * @property string \$name
+     *
+     * @property string \$email
+     *
+     * @return string
+     */
+    final public function doSomething(string \$name, string \$email): string
+    {
+        return "@author \$name <@email>";
+    }
+EOL;
+
+        $this->assertSame($expected, $this->generator->generate($method));
+    }
+
+    public function testMethodWithDocBlock(): void
+    {
+        $method = new Method(
+            'someMethod',
+            new Flag(Flag::FLAG_ABSTRACT, Flag::FLAG_STATIC),
+            null,
+            null,
+            new DocBlock(
+                'Some description',
+                null,
+                [new Tag('throws', InvalidArgumentException::class)]
+            ),
+            new Type(Type::TYPE_VOID)
+        );
+
+        $expected = <<<EOL
+    /**
+     * Some description
+     *
+     * @throws InvalidArgumentException
+     *
+     * @return void
+     */
+    abstract public static function someMethod(): void;
+EOL;
+
+        $this->assertSame($expected, $this->generator->generate($method));
+    }
+
+    public function testMethodWithArgumentsAndDocBlock(): void
+    {
+        $method = new Method(
+            'doSomething',
+            null,
+            [
+                new Argument('name', new Type(Type::TYPE_STRING), new Value()),
+                new Argument('email', new Type(Type::TYPE_STRING), new Value()),
+            ],
+            'return "@author $name <@email>";',
+            new DocBlock(
+                'Some description',
+                null,
+                [new Tag('throws', InvalidArgumentException::class)]
+            ),
+            new Type(Type::TYPE_STRING)
+        );
+
+        $expected = <<<EOL
+    /**
+     * Some description
+     *
+     * @property string \$name
+     *
+     * @property string \$email
+     *
+     * @throws InvalidArgumentException
+     *
+     * @return string
+     */
+    public function doSomething(string \$name, string \$email): string
+    {
+        return "@author \$name <@email>";
+    }
+EOL;
+
+        $this->assertSame($expected, $this->generator->generate($method));
+    }
+}
