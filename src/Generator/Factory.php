995a87d6b987e20ebced948765bc922184b683d7
Igor Vuckovic <igor.vuckovic@arbor-education.com>
2019-11-19 15:15:05 +0100

    Added Factory Generator

---
+++ src/Generator/Factory.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Leaditin\Code\Generator;
+
+/**
+ * @package Leaditin\Code
+ * @author Igor Vuckovic <igor@vuckovic.biz>
+ * @license MIT
+ */
+class Factory
+{
+    /**
+     * @return ArgumentGenerator
+     */
+    public function argumentGenerator(): ArgumentGenerator
+    {
+        return new ArgumentGenerator(
+            $this->typeGenerator(),
+            $this->valueGenerator()
+        );
+    }
+
+    /**
+     * @return ConstantGenerator
+     */
+    public function constantGenerator(): ConstantGenerator
+    {
+        return new ConstantGenerator(
+            $this->valueGenerator(),
+            $this->visibilityGenerator()
+        );
+    }
+
+    /**
+     * @return DocBlockGenerator
+     */
+    public function docBlockGenerator(): DocBlockGenerator
+    {
+        return new DocBlockGenerator(
+            $this->tagGenerator()
+        );
+    }
+
+    /**
+     * @return MethodGenerator
+     */
+    public function methodGenerator(): MethodGenerator
+    {
+        return new MethodGenerator(
+            $this->argumentGenerator(),
+            $this->docBlockGenerator(),
+            $this->typeGenerator(),
+            $this->visibilityGenerator()
+        );
+    }
+
+    /**
+     * @return PropertyGenerator
+     */
+    public function propertyGenerator(): PropertyGenerator
+    {
+        return new PropertyGenerator(
+            $this->docBlockGenerator(),
+            $this->typeGenerator(),
+            $this->valueGenerator(),
+            $this->visibilityGenerator()
+        );
+    }
+
+    /**
+     * @return TagGenerator
+     */
+    public function tagGenerator(): TagGenerator
+    {
+        return new TagGenerator();
+    }
+
+    /**
+     * @return TraitGenerator
+     */
+    public function traitGenerator(): TraitGenerator
+    {
+        return new TraitGenerator(
+            $this->constantGenerator(),
+            $this->docBlockGenerator(),
+            $this->methodGenerator(),
+            $this->propertyGenerator()
+        );
+    }
+
+    /**
+     * @return TypeGenerator
+     */
+    public function typeGenerator(): TypeGenerator
+    {
+        return new TypeGenerator();
+    }
+
+    /**
+     * @return ValueGenerator
+     */
+    public function valueGenerator(): ValueGenerator
+    {
+        return new ValueGenerator();
+    }
+
+    /**
+     * @return VisibilityGenerator
+     */
+    public function visibilityGenerator(): VisibilityGenerator
+    {
+        return new VisibilityGenerator();
+    }
+}
---
+++ tests/Generator/FactoryTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Test\Leaditin\Code\Generator;
+
+use Leaditin\Code\Generator\ArgumentGenerator;
+use Leaditin\Code\Generator\ConstantGenerator;
+use Leaditin\Code\Generator\DocBlockGenerator;
+use Leaditin\Code\Generator\Factory;
+use Leaditin\Code\Generator\MethodGenerator;
+use Leaditin\Code\Generator\PropertyGenerator;
+use Leaditin\Code\Generator\TagGenerator;
+use Leaditin\Code\Generator\TraitGenerator;
+use Leaditin\Code\Generator\TypeGenerator;
+use Leaditin\Code\Generator\ValueGenerator;
+use Leaditin\Code\Generator\VisibilityGenerator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @package Test\Leaditin\Code
+ * @author Igor Vuckovic <igor@vuckovic.biz>
+ *
+ * @covers \Leaditin\Code\Generator\Factory
+ */
+final class FactoryTest extends TestCase
+{
+    /**
+     * @var Factory
+     */
+    private $factory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->factory = new Factory();
+    }
+
+    public function generatorNameDataProvider(): array
+    {
+        return [
+            [
+                'methodName' => 'argumentGenerator',
+                'expected' => ArgumentGenerator::class,
+            ],
+            [
+                'methodName' => 'constantGenerator',
+                'expected' => ConstantGenerator::class,
+            ],
+            [
+                'methodName' => 'docBlockGenerator',
+                'expected' => DocBlockGenerator::class,
+            ],
+            [
+                'methodName' => 'methodGenerator',
+                'expected' => MethodGenerator::class,
+            ],
+            [
+                'methodName' => 'propertyGenerator',
+                'expected' => PropertyGenerator::class,
+            ],
+            [
+                'methodName' => 'tagGenerator',
+                'expected' => TagGenerator::class,
+            ],
+            [
+                'methodName' => 'traitGenerator',
+                'expected' => TraitGenerator::class,
+            ],
+            [
+                'methodName' => 'typeGenerator',
+                'expected' => TypeGenerator::class,
+            ],
+            [
+                'methodName' => 'valueGenerator',
+                'expected' => ValueGenerator::class,
+            ],
+            [
+                'methodName' => 'visibilityGenerator',
+                'expected' => VisibilityGenerator::class,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider generatorNameDataProvider
+     *
+     * @param string $methodName
+     * @param string $expected
+     */
+    public function testArgumentGenerator(string $methodName, string $expected): void
+    {
+        $this->assertInstanceOf($expected, $this->factory->$methodName());
+    }
+}
